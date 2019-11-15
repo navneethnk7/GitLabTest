2b48ac6524a5f4ebea94e80e5d0abc671215c5b5
krish <krishnav199912@gmail.com>
2019-11-15 16:50:52 +0530

    revised

--- menu.py
+++ menu.py
@@ -2,8 +2,17 @@ import addition
 import multiplication
 import subtract
 import division
+print("1: Addition, 2: Subtraction, 3: Multiplication, 4: Division")
 
-addition.add()
-multiplication.multiplication()
-subtract.subtraction()
-division.division()
+inp = input("Enter the choice: ")
+
+if inp == 1:
+	addition.add()
+elif inp == 3:
+	multiplication.multiplication()
+elif inp == 2:
+	subtract.subtraction()
+elif inp == 4:
+	division.division()
+else:
+	print("Invalid input")
